cb17db209fb513ed9dee09503ed4ba599d8806fb
tj <tjkainul@gmail.com>
2019-03-25 22:27:24 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here jeah hell
-        // write your code here huh hell
-        // write your code here jooh hell
-        // write your code here yeah hell
+        // write your code here
     }
 }
